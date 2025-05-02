e7e1d190362b6e211c1aef3e7094707419b63d21
Lucas Eduardo <lucaseduardo3528@hotmail.com>
2025-05-01 22:35:03 -0300

    type: implementing barber data

--- app/Data/BarberData.php
+++ app/Data/BarberData.php
@@ -8,6 +8,7 @@ use Spatie\LaravelData\Data;
 class BarberData extends Data
 {
     public function __construct(
+        public int $id,
         public string $name,
         public string $tel,
     )
@@ -17,6 +18,7 @@ class BarberData extends Data
     public static function fromUser (User $user): self
     {
         return new self (
+            id: $user->id,
             name: $user->name,
             tel: $user->tel,
         );
